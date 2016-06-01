950c287a022dac3be121d53947201e0ad1d94109
Marco Arena <marcoarena@email.it>
2016-06-01 11:18:59 +0200

    Create isprime.h

---
+++ math/isprime.h
@@ -0,0 +1,25 @@
+/*
+  simple function to check if an int number is prime
+*/
+bool isPrime(int num) 
+{
+    if (num <= 3) 
+    {
+        return num > 1;
+    }
+    if (num % 2 == 0 || num % 3 == 0) 
+    {
+        return false;
+    } 
+    else 
+    {
+        for (int i = 5; i * i <= num; i += 6) 
+        {
+            if (num % i == 0 || num % (i + 2) == 0) 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
